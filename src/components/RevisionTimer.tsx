8ccf26a9c0a2ebe5ba6e399623324b9431f27853
Ben Ward <bw@ben-ward.net>
2017-07-22 21:51:57 +0100

    TaskList

---
+++ src/components/RevisionTimer.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { TaskList } from "./TaskList";
+
+// 'HelloProps' describes the shape of props.
+// State is never set so we use the 'undefined' type.
+export class RevisionTimer extends React.Component<undefined, undefined> {
+    render() {
+        return(
+            <div>
+                <h1>Morwenna's Revision Nerve Centre</h1>
+                <p>What's up</p>
+                <TaskList />
+            </div>
+        );
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/Hello";
+import { RevisionTimer } from "./components/RevisionTimer";
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+    <RevisionTimer />,
+    document.getElementById("root")
 );
