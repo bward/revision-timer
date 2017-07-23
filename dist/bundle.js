f4bc1537e49792af87629d2fb4e9f55ca88f330f
Ben Ward <bw@ben-ward.net>
2017-07-23 23:08:05 +0100

    Actual times

--- dist/bundle.js
+++ dist/bundle.js
@@ -79,7 +79,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const React = __webpack_require__(0);
 const ReactDOM = __webpack_require__(2);
 const RevisionTimer_1 = __webpack_require__(3);
-ReactDOM.render(React.createElement(RevisionTimer_1.RevisionTimer, { workSeconds: 15, breakSeconds: 5 }), document.getElementById("root"));
+ReactDOM.render(React.createElement(RevisionTimer_1.RevisionTimer, { workSeconds: 1500, breakSeconds: 300 }), document.getElementById("root"));
 
 
 /***/ }),
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,6 @@ import * as ReactDOM from "react-dom";
 import { RevisionTimer } from "./components/RevisionTimer";
 
 ReactDOM.render(
-  <RevisionTimer workSeconds={15} breakSeconds={5}/>,
+  <RevisionTimer workSeconds={1500} breakSeconds={300}/>,
   document.getElementById("root")
 );
